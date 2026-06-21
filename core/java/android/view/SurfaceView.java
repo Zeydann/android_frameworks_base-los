AxionAOSP/android_frameworks_base

[bugfix] fixing ignore secure window flags
Change-Id: I1b3342d927b72173ba41f1ab58b901fd7e1ab7b8
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

core/java/android/view/SurfaceView.java
@@ -52,6 +52,7 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.AttributeSet;
@@ -953,7 +954,9 @@ public class SurfaceView extends View implements ViewRootImpl.SurfaceChangedCall
      * @param isSecure True if the surface view is secure.
      */
     public void setSecure(boolean isSecure) {
-        if (isSecure) {
+        boolean ignoreSecure = Settings.Secure.getInt(getContext().getContentResolver(),
+                "window_ignore_secure", 0) == 1;
+        if (isSecure && !ignoreSecure) {
             mSurfaceFlags |= SurfaceControl.SECURE;
         } else {
             mSurfaceFlags &= ~SurfaceControl.SECURE;

core/java/android/view/WindowManagerGlobal.java
@@ -40,6 +40,7 @@ import android.os.Looper;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
+import android.provider.Settings;
 import android.util.AndroidRuntimeException;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -511,6 +512,14 @@ public final class WindowManagerGlobal {
                         windowlessSession, new WindowlessWindowLayout());
             }
 
+            boolean ignoreSecure = Settings.Secure.getInt(
+                    view.getContext().getContentResolver(),
+                    "window_ignore_secure", 0) == 1;
+
+            if (ignoreSecure) {
+                wparams.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
+            }
+
             view.setLayoutParams(wparams);
 
             int visibleRootCount = 0;
@@ -545,6 +554,14 @@ public final class WindowManagerGlobal {
         }
         if (!(params instanceof WindowManager.LayoutParams wparams)) {
             throw new IllegalArgumentException("Params must be WindowManager.LayoutParams");
+        }
+
+        boolean ignoreSecure = Settings.Secure.getInt(
+                view.getContext().getContentResolver(),
+                "window_ignore_secure", 0) == 1;
+
+        if (ignoreSecure) {
+            wparams.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
         }
 
         view.setLayoutParams(wparams);

services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10882,7 +10882,8 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     boolean getDisableSecureWindows() {
-        return mDisableSecureWindows;
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                "window_ignore_secure", 0) == 1 || mDisableSecureWindows;
     }
 
     /**

Zeydann/android_frameworks_base-los

Signed-off-by: Zeydann <zeydann.sc@gmail.com>

core/java/android/view/SurfaceView.java
@@ -52,6 +52,7 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.AttributeSet;
@@ -953,7 +954,9 @@ public class SurfaceView extends View implements ViewRootImpl.SurfaceChangedCall
      * @param isSecure True if the surface view is secure.
      */
     public void setSecure(boolean isSecure) {
-        if (isSecure) {
+        boolean ignoreSecure = Settings.Global.getInt(getContext().getContentResolver(),
+                "window_ignore_secure", 0) == 1;
+        if (isSecure && !ignoreSecure) {
             mSurfaceFlags |= SurfaceControl.SECURE;
         } else {
             mSurfaceFlags &= ~SurfaceControl.SECURE;

core/java/android/view/WindowManagerGlobal.java
@@ -40,6 +40,7 @@ import android.os.Looper;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
+import android.provider.Settings;
 import android.util.AndroidRuntimeException;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -480,6 +481,14 @@ public final class WindowManagerGlobal {
                         windowlessSession, new WindowlessWindowLayout());
             }
 
+            boolean ignoreSecure = Settings.Global.getInt(
+                    view.getContext().getContentResolver(),
+                    "window_ignore_secure", 0) == 1;
+
+            if (ignoreSecure) {
+                wparams.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
+            }
+
             view.setLayoutParams(wparams);
 
             mViews.add(view);
@@ -508,6 +517,14 @@ public final class WindowManagerGlobal {
         }
         if (!(params instanceof WindowManager.LayoutParams wparams)) {
             throw new IllegalArgumentException("Params must be WindowManager.LayoutParams");
+        }
+
+        boolean ignoreSecure = Settings.Global.getInt(
+                view.getContext().getContentResolver(),
+                "window_ignore_secure", 0) == 1;
+
+        if (ignoreSecure) {
+            wparams.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
         }
 
         view.setLayoutParams(wparams);

services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10881,7 +10881,8 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     boolean getDisableSecureWindows() {
-        return mDisableSecureWindows;
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                "window_ignore_secure", 0) == 1 || mDisableSecureWindows;
     }
 
     /**